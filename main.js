07ff3f115665731cc98f828652d5f4974c1cc315
sutirthverma <sutirthverma@yahoo.com>
2024-07-09 23:33:07 +0530

    prompts for input

--- main.js
+++ main.js
@@ -1,22 +1,45 @@
 const fs = require('fs');
 const path = require('path');
 const readline = require('readline');
+const prompt = require('prompts');
 
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
-})
+});
 
+async function takeInput(){
+    let response = await prompt({
+        type: 'number',
+        name: 'value',
+        message: 'Enter 1 to Make Directory\nEnter Any Other Key To Exit.'
+    });
 
-rl.question('Enter directory name: ', (dirName) => 
-    fs.mkdir(path.join(__dirname, dirName),cb)
-);
+    switch (response.value){
+        case 1:
+            makeDirec();
+            break;
 
-function cb(err){
-    if(err){
-        return console.log(err);
-    }else{
-        console.log('Directory created successfully');
-    }
+        default: 
+            console.log('Exiting...');                
+    }    
 }
 
+
+takeInput();
+
+
+
+// function makeDirec(){
+//     rl.question('Enter directory name: ', (dirName) => 
+//         fs.mkdir(path.join(__dirname, dirName),cb)
+//     );
+    
+//     function cb(err){
+//         if(err){
+//             return console.log(err);
+//         }else{
+//             console.log('Directory created successfully');
+//         }
+//     }    
+// }
