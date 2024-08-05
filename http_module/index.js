ca54e6ba5a98cec08c0379742f84de5504e4015b
sutirthverma <sutirthverma@yahoo.com>
2024-08-05 08:23:38 +0530

    url package

--- http_module/index.js
+++ http_module/index.js
@@ -1,26 +1,33 @@
 const http = require('http');
 const fs = require('fs');
+const url = require('url');
 
 let count = 0;
 
 const server = http.createServer((req, res) => {
+    if (req.url == '/favicon.ico') return res.end();
+
     count++;
     const log = `\n${Date.now()}: New Request Recieved\n ${req.url}`;
 
-    if (req.url != '/favicon.ico') {
-        fs.appendFile('demo.txt', log, (err, data) => {
-            switch (req.url) {
-                case '/':
-                    res.end('Homepage');
-                    break;
-                case '/about-us':
-                    res.end(`I'm Sutirth Verma`);
-                    break;
-                default:
-                    res.end(http.STATUS_CODES['404']);
-            }
-        });
-    }
+    const myUrl = url.parse(req.url, true);
+    console.log(`Url: ${JSON.stringify(myUrl)}`);    
+
+    fs.appendFile('demo.txt', log, (err, data) => {
+        switch (myUrl.pathname) {
+            case '/':
+                res.end('Homepage');
+                break;
+            case '/about-us':
+                const username = myUrl.query.name;
+                const age = myUrl.query.age;
+                res.end(`I'm ${username} and my age is ${age}`);
+                break;
+            default:
+                res.end(http.STATUS_CODES['404']);
+        }
+    });
+
 });
 
 server.listen(8000, () => console.log('Server started'));
